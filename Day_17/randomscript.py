de4522e7ddab38fa0f375a8a46e56f5397dd24a5
Ody Nwagwu (260004669) <Ody.Nwagwu@gevernova.com>
2025-11-14 10:58:20 +0000

    changes

---
+++ Day_17/randomscript.py
@@ -0,0 +1,7 @@
+import sys
+
+print(f"Hello {sys.argv[1]}")
+print(f"this is just random stuff here a text \n {sys.argv[2]}")
+print(f"The toggle of this value is {sys.argv[3]}")
+print(f"Your choice is {sys.argv[4]}")
+print(f"Finally the password is {sys.argv[5]}")
